d399a55b083af377c8728e2acf73e2aa3fa17136
ispasdani <ispasdani14@gmail.com>
2021-11-17 23:40:47 +0100

    Project Pushed

---
+++ app.js
@@ -0,0 +1,48 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+let navbar = document.querySelector(".navbar");
+
+// Get the offset position of the navbar
+let sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+// This function is for the popup image on click event
+document.querySelectorAll('.img-gallery').forEach(image => {
+    image.onclick = () => {
+        document.querySelector('.popup-image').style.display = 'block';
+        document.querySelector('.popup-image img').src = image.getAttribute('src');
+    }
+})
+
+document.querySelector('.popup-image span').onclick = () => {
+    document.querySelector('.popup-image').style.display = 'none';
+}
+
+// This function is for the HAMBURGER MENU
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const navBtn = document.querySelectorAll(".nav-a");
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navLinks.classList.toggle("active");
+}
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navLinks.classList.remove("active");
+}
+
+hamburger.addEventListener("click", mobileMenu);
+navBtn.forEach(n => n.addEventListener("click", closeMenu));
+
